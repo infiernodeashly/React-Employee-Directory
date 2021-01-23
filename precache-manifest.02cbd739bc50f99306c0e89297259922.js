ecb275f3c740acc65daafe2e79b61e895669b7d6
Ashly Mayle <infiernodeashly@gmail.com>
2021-01-22 21:24:03 -0500

    Updates

---
+++ precache-manifest.02cbd739bc50f99306c0e89297259922.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "722b258300d151e4c5219fa331f8bdc6",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "56ad9226ad23678b6525",
+    "url": "/React-Employee-Directory/static/css/main.8d657a30.chunk.css"
+  },
+  {
+    "revision": "80c4605b02c7104755d3",
+    "url": "/React-Employee-Directory/static/js/2.5d6b6967.chunk.js"
+  },
+  {
+    "revision": "56ad9226ad23678b6525",
+    "url": "/React-Employee-Directory/static/js/main.55b8835b.chunk.js"
+  },
+  {
+    "revision": "7e48c29bac8a0400fb02",
+    "url": "/React-Employee-Directory/static/js/runtime-main.ddec196d.js"
+  }
+]);
